902efed156e4e425e793dae98c95a43bc488c1ab
dm4brl <92757328+dm4brl@users.noreply.github.com>
2024-02-15 22:38:54 -0300

    Create  Form.vue

---
+++ frontend/src/ components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="expression" placeholder="Enter expression">
+    <button @click="submitExpression">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expression: ''
+    };
+  },
+  methods: {
+    submitExpression() {
+      // Implement logic to submit expression to the backend
+    }
+  }
+};
+</script>
