bae32f9b180a997020f89cc89eff435a2189d73e
dm4brl <92757328+dm4brl@users.noreply.github.com>
2024-02-16 12:00:27 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+git clone https://github.com/dm4brl/distributed-calculator.git
+
+cd distributed-calculator
+
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+POSTGRES_DB=distributed_calculator
+POSTGRES_HOST=postgres
+POSTGRES_PORT=5432
+
+REDIS_HOST=redis
+REDIS_PORT=6379
+
+RABBITMQ_HOST=rabbitmq
+RABBITMQ_PORT=5672
+
+docker-compose up -d
+
+go get -u github.com/golang/dep/cmd/dep
+dep ensure
+
 go build -o server ./cmd/server/main.go
 go build -o agent ./cmd/agent/main.go
+
+
+./server
+
+./agent
+
+
