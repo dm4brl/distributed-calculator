c8b6a4830f8ff49074dc07b2f38fa52e69c035fb
dm4brl <92757328+dm4brl@users.noreply.github.com>
2024-02-15 22:41:52 -0300

    Create ExpressionsList.vue

---
+++ frontend/src/ components/ExpressionsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Expressions List</h2>
+    <ul>
+      <li v-for="expression in expressions" :key="expression.id">
+        {{ expression.text }} - {{ expression.status }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expressions: [] // Array to hold expressions fetched from backend
+    };
+  },
+  created() {
+    // Implement logic to fetch expressions from backend
+  }
+};
+</script>
